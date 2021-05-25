46ad8f179d6415a6fdbf34a43d0af22224476da6
russ13 <contactrussellmoore@gmail.com>
2021-05-25 14:17:41 +1000

    Added customer delete

--- LoyaltyApp.rb
+++ LoyaltyApp.rb
@@ -100,3 +100,27 @@ while
         password = prompt.mask("Please Enter Staff Password?")
     end
 end
+
+puts " "
+
+exit = false
+
+while exit == false
+
+        choices = %w(Create\ Customer View\ Customer View\ Birthdays Book\ Customer Delete\ Customer Exit)
+        answer = prompt.enum_select("What are we doing today?", choices)
+
+    if answer == "Create Customer"
+        new_customer
+            elsif answer == "View Customer"
+                view_customer
+            elsif answer == "View Birthdays"
+                view_birthdays
+            elsif answer == "Book Customer"
+                new_booking
+            elsif answer == "Delete Customer"
+                delete_customer
+            elsif answer == "Exit"
+                exit = true
+    end
+end
