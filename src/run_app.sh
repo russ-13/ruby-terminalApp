b4c8a679a5e4f30c69b405f8cb32f678965f15d7
russ13 <contactrussellmoore@gmail.com>
2021-05-29 13:30:05 +1000

    Script File

---
+++ src/run_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STR0= $'\nLet's First Install the nessisary Gems for this appliation to work!\n'
+
+echo "$STR0"
+
+bundle install
+
+STR1= $'\nReady To go!\n'
+
+echo "$STR1"
+
+clear
+
+ruby main.rb
+
+
+
+
